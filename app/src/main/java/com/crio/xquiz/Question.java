8502df0dab709327fc7fbcb7a98afc38cb737f81
Udit Ranjan <101802436+Phonix10@users.noreply.github.com>
2025-05-10 09:42:52 +0530

    adding question con

--- app/src/main/java/com/crio/xquiz/Question.java
+++ app/src/main/java/com/crio/xquiz/Question.java
@@ -5,6 +5,12 @@ public class Question{
     //TODO: Define the variable to store the question text
     //TODO: Define the variable to store the answer text
     //TODO: Define the variable to store the list of text choices
+    protected String questionText;
+    protected String answer;
+    protected List<String> choices;
+
+
+
 
     // TODO: Create the Question class constructor 
     // use the  signature :  public Question(String questionText, List<String> choices, String answer)
@@ -33,7 +39,29 @@ public class Question{
 
     //TODO: Create the method checkAnswer() with signature: public boolean checkAnswer(String answer)
         // Validate: If answer provided in the input matches the existing answer for the given question, then return True
+        
         // Else:  return False.
+
+        public Question(String questionText, List<String> choices, String answer) {
+                if(questionText == null || questionText.isEmpty()){
+                        System.out.println("Question text cannot be null or empty!");
+                }
+                else if(choices == null || choices.isEmpty()){
+                        System.out.println("choices cannot be null or empty!");
+                }
+                else if(answer == null || answer.isEmpty()){
+                        System.out.println("Answer cannot be null or empty!");
+                }
+                else if(!choices.contains(answer)){
+                        System.out.println("Answer is not present among the choices!");
+                }
+                else{
+                        this.questionText = questionText;
+                        this.choices = choices;
+                        this.answer = answer;
+                }
+
+        }
 
 
 public void display(){
